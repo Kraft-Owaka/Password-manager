a42fdd7974d9ff8cbbf86eab5e98972d4e9a6c48
kraft-owaka <owakakraft@gmail.com>
2020-01-24 04:01:24 +0300

    helps us find out if the methods are initialized properly

--- run.py
+++ run.py
@@ -1,2 +1,24 @@
 import unittest
-import password
+from password import User 
+import pyperclip
+from password import Account
+
+class TeatUser(unittest.TestCase):#create subclass to inherit from unittest.TestCase
+
+    def setup(self):
+        self.new_credentials = User("Owaka", "twitter", "pass@987ko")# create contact object
+        self.new_account = Account("Kraft", "qwe12pr")
+
+    def test_init(self):
+        self.assertEqual(self.new)
+
+
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.mail())
